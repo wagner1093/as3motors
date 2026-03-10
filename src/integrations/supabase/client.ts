f91531dac65ad5fc43dbcdeaee07443c755c9a47
wagner1093 <wagner.1093@gmail.com>
2026-03-10 00:14:36 -0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = "https://oxisdjygwxhikkixqpap.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94aXNkanlnd3hoaWtraXhxcGFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwODI0NzYsImV4cCI6MjA4ODY1ODQ3Nn0.iXmceArt6OI_sW-jLknmL-HQU4quyOgkSDN4Cc73T5U";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94aXNkanlnd3hoaWtraXhxcGFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwODI0NzYsImV4cCI6MjA4ODY1ODQ3Nn0.iXmceArt6OI_sW-jLknmL-HQU4quyOgkSDN4Cc73T5U";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: localStorage,
     persistSession: true,
